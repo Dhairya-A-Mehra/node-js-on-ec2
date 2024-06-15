c6e2b48cdcf7e51ce0735b8ea18d963ac32e358c
Dhairya <mehra.dhairya.btech2022@sitpune.edu.in>
2024-06-15 14:22:36 +0530

    files uploaded

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -92,11 +92,4 @@ resource "aws_alb_target_group" "hello_world_tg" {
 
 resource "aws_alb_listener" "hello_world_listener" {
   load_balancer_arn = aws_alb.hello_world_alb.arn
-  port              = "80"
-  protocol          = "HTTP"
-
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_alb_target_group.hello_world_tg.arn
-  }
-}
+  po
---
+++ Terraform/terraform.tfvars
@@ -0,0 +1,4 @@
+aws_region = "us-east-1"
+subnet_ids = ["subnet-xxxxxxxx", "subnet-xxxxxxxx"]
+security_group_ids = ["sg-xxxxxxxx"]
+vpc_id = "vpc-xxxxxxxx"
