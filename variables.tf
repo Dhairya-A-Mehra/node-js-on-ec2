71e64b4ca8d67b502326fb2bfc078abe56593f38
Dhairya <mehra.dhairya.btech2022@sitpune.edu.in>
2024-06-15 14:16:42 +0530

    files uploaded

--- variables.tf
+++ variables.tf
@@ -1,7 +1,7 @@
 variable "aws_region" {
   description = "The AWS region to deploy resources in"
   type        = string
-  default     = "us-west-2"
+  default     = "us-east-1"
 }
 
 variable "subnet_ids" {
